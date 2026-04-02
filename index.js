1d2631b2504ba56dcd94e006fac4f843ab02f68d
Tagore9930 <tagore9930@gmail.com>
2026-04-02 23:50:06 +0530

    Completed Topic: How TypeScript Helps You Catch Errors

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello, World!");
+const students = [
+    {
+        name: "Bharath",
+        score: 70,
+    },
+    {
+        name: "Rohit",
+        score: 90,
+    },
+    {
+        name: "Tagore",
+        score: 50,
+    }
+];
+for (const student of students) {
+    console.log("Name:", student.name);
+    console.log("Score:", student.score);
+    // console.log("Score:", student.grade);
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello, World!");
+const students = [
+    {
+        name: "Bharath",
+        score: 70,
+    },
+    {
+        name: "Rohit",
+        score: 90,
+    },
+    {
+        name: "Tagore",
+        score: 50,
+    }
+];
+
+for (const student of students) {
+    console.log("Name:", student.name);
+    console.log("Score:", student.score);
+    // console.log("Score:", student.grade);
+}
