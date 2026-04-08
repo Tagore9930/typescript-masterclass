94cfcada39e70367e2295f8881efc091b7aa20ce
Tagore9930 <tagore9930@gmail.com>
2026-04-08 18:00:04 +0530

    Completed Topic: string, number and boolean

--- index.ts
+++ index.ts
@@ -26,6 +26,7 @@ for (const student of students) {
 */
 
 
+/* //////////////////////////////////////
 // Your First TypeScript Program
 
 // @ts-ignore
@@ -35,3 +36,29 @@ function addNumbers(a: number, b: number): number {
 
 console.log(addNumbers(2, 3));
 // console.log(addNumbers("text", 3));
+*/
+
+// string, number and boolean
+
+/** String types */
+
+var firstName: string = "Tagore";
+firstName = 1;
+let automobile = "BMW";
+const city = "India";
+let students = 32;
+let studentAsString: string = students.toString();
+
+
+/** Number types */
+var age = 20;
+let year = 2004;
+const numberOfMembers = 61;
+numberOfMembers = 128;
+let stringToNumber: number = parseInt('1998');
+
+/** Booleans */
+
+let isStudent: boolean = false;
+const alwaysStudent = true;
+let minimumAge: boolean = age >= 6 ? true : false;
